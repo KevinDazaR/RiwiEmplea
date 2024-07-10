f2f65e811a22e193ef5dea9fe452e58c7b22d863
Jhoan Rios <jhoanriosmorales@mgmail.com>
2024-07-10 15:07:01 -0500

    configuracion proyecto

---
+++ Applications/Services/Token/NewClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiwiEmplea.Applications.Services.Token
+{
+    public class NewClass
+    {
+        
+    }
+}
