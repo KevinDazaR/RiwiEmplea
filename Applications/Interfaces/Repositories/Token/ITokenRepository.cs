735a60ad154c9dc6fdb3f8f6b31249e7de18a9d7
Jhoan Rios <jhoanriosmorales@mgmail.com>
2024-07-10 16:09:36 -0500

    Creacion de funcion que genera JWT y la configuracion sin login

--- Applications/Interfaces/Repositories/Token/ITokenRepository.cs
+++ Applications/Interfaces/Repositories/Token/ITokenRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RiwiEmplea.Models;
 
 namespace RiwiEmplea.Applications.Interfaces.Repositories.Token
 {
-    public interface NewInterface
+    public interface ITokenRepository
     {
-        
+        string GetToken(User user);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RiwiEmplea.Applications.Interfaces.Repositories.Token;
+using RiwiEmplea.Applications.Services.Token;
 using RiwiEmplea.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +13,48 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Inyeccion de depenencias 
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+
 builder.Services.AddDbContext<BaseContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("MySqlConnection"),
         Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
 
+/* Configuracion del token */
+builder.Services.AddAuthentication(opt => {
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+        .AddJwtBearer(configure => {
+            configure.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+/* Control de errores del token */
+            configure.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    if (context.Exception is SecurityTokenExpiredException)
+                    {
+                        Console.WriteLine("Token expirado, Porfavor genere uno nuevo.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Usuario no autorizado.");
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
 
 var app = builder.Build();
 
